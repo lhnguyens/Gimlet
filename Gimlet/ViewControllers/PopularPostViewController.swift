a70fa99c383c563ca2122a6a3610ab1f8f1ec278
Luan Nguyen <lh.nguyens@gmail.com>
2019-11-06 10:39:35 +0100

    Retrieving user data completed

--- Gimlet/ViewControllers/PopularPostViewController.swift
+++ Gimlet/ViewControllers/PopularPostViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import OAuthSwift
+import Alamofire
 
 class PopularPostViewController: UIViewController {
     
@@ -16,35 +17,42 @@ class PopularPostViewController: UIViewController {
     var newOAuthToken: String!
     
     let state = "GimletRequest"
-    let scope = "mysubreddits, read, report, save, submit, subscribe, vote"
+    let scope = "identity, edit, flair, history, modconfig, modflair, modlog, modposts, modwiki, mysubreddits, privatemessages, read, report, save, submit, subscribe, vote, wikiedit, wikiread"
     let gimletAuth = "Gimlet://oauth-callback"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         //AUTHENTHICATION
-        oauthswift = OAuth2Swift(consumerKey: "wrQwzbsIGn4Dlg", consumerSecret: "", authorizeUrl: "https://www.reddit.com/api/v1/authorize.compact?client_id=wrQwzbsIGn4Dlg&redirect_uri=\(gimletAuth)&duration=temporary", responseType: "token")
-           
-               handle = oauthswift.authorize(
-                   withCallbackURL: URL(string: gimletAuth)!,
-                   scope: scope, state:state) { result in
-                   switch result {
-                   case .success(let (credential, response, parameters)):
-                     print(credential.oauthToken)
-                    // Do Request
-                   case .failure(let error):
-                       print("FAILED")
-                       print(error.localizedDescription)
-                   }
-                    
-            }
+        oauthswift = OAuth2Swift(consumerKey: "wrQwzbsIGn4Dlg", consumerSecret: "", authorizeUrl: "https://www.reddit.com/api/v1/authorize.compact?client_id=wrQwzbsIGn4Dlg&redirect_uri=\(gimletAuth)", accessTokenUrl:  "https://www.reddit.com/api/v1/access_token", responseType: "token")
+        
+        handle = oauthswift.authorize(
+            withCallbackURL: URL(string: gimletAuth)!,
+            scope: scope, state:state) { result in
+                switch result {
+                case .success(let (credential, response, parameters)):
+                    print(credential.oauthToken)
+                    let _  = self.oauthswift.client.get("https://oauth.reddit.com") { result in
+                        switch result {
+                        case .success(let response):
+                           let dataJSON = try? response.jsonObject()
+                            print(String(describing: dataJSON))
+                        case .failure(let error):
+                            print(error)
+                        }
+                    }
+                case .failure(let error):
+                    print("FAILED")
+                    print(error.localizedDescription)
+                }
+                
+        }
         view.backgroundColor = .white
         configureNavigationBar(largeTitleColor: .white, backgoundColor: UIColor(red:0.47, green:0.11, blue:0.86, alpha:1.0), tintColor: .clear, title: "Popular", preferredLargeTitle: true)
         
     }
     
-
+    
     
     
     
