a9b3d3b956bd69dbf04bad5a8becbebd8eb377a4
Luan Nguyen <lh.nguyens@gmail.com>
2019-11-06 09:40:14 +0100

    Successful authenthication

---
+++ Gimlet/Reddit/Authenthication.swift
@@ -0,0 +1,30 @@
+//
+//  Authenthication.swift
+//  Gimlet
+//
+//  Created by Luan Nguyen on 2019-10-30.
+//  Copyright © 2019 Luan Nguyen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import OAuthSwift
+
+class Authenthication {
+    
+//    let oAuthSwift = OAuth2Swift(consumerKey: "wrQwzbsIGn4Dlg", consumerSecret: "", authorizeUrl: "https://www.reddit.com/api/v1/access_token", responseType: "access_token")
+//
+//
+//    let handle = oAuthSwift.authorize(
+//        withCallbackURL: URL(string: "Gimlet://oauth-callback")!,
+//        scope: "scopes", state:"REDDIT") { result in
+//        switch result {
+//        case .success(let (credential, response, parameters)):
+//          print(credential.oauthToken)
+//          // Do your request
+//        case .failure(let error):
+//          print(error.localizedDescription)
+//        }
+//    }
+    
+}
--- Gimlet/ViewControllers/PopularPostViewController.swift
+++ Gimlet/ViewControllers/PopularPostViewController.swift
@@ -7,21 +7,44 @@
 //
 
 import UIKit
+import OAuthSwift
 
 class PopularPostViewController: UIViewController {
     
+    var oauthswift: OAuth2Swift!
+    var handle: OAuthSwiftRequestHandle!
+    var newOAuthToken: String!
     
-    
-    
-    
+    let state = "GimletRequest"
+    let scope = "mysubreddits, read, report, save, submit, subscribe, vote"
+    let gimletAuth = "Gimlet://oauth-callback"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        //AUTHENTHICATION
+        oauthswift = OAuth2Swift(consumerKey: "wrQwzbsIGn4Dlg", consumerSecret: "", authorizeUrl: "https://www.reddit.com/api/v1/authorize.compact?client_id=wrQwzbsIGn4Dlg&redirect_uri=\(gimletAuth)&duration=temporary", responseType: "token")
+           
+               handle = oauthswift.authorize(
+                   withCallbackURL: URL(string: gimletAuth)!,
+                   scope: scope, state:state) { result in
+                   switch result {
+                   case .success(let (credential, response, parameters)):
+                     print(credential.oauthToken)
+                    // Do Request
+                   case .failure(let error):
+                       print("FAILED")
+                       print(error.localizedDescription)
+                   }
+                    
+            }
         view.backgroundColor = .white
         configureNavigationBar(largeTitleColor: .white, backgoundColor: UIColor(red:0.47, green:0.11, blue:0.86, alpha:1.0), tintColor: .clear, title: "Popular", preferredLargeTitle: true)
-
+        
     }
     
+
     
     
     
